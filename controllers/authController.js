f7517c329193f36a17a678259a496529b344041c
BhavyaSreeJallolla <bhavyasreejallolla967@gmail.com>
2024-09-29 09:59:06 +0530

    Switched from bcrypt to bcryptjs

--- controllers/authController.js
+++ controllers/authController.js
@@ -1,6 +1,6 @@
 // controllers/authController.js
 const User = require('../models/userModel');
-const bcrypt = require('bcrypt');
+const bcrypt = require('bcryptjs');  // Change this to bcryptjs
 const jwt = require('jsonwebtoken');
 
 // User registration
@@ -8,6 +8,7 @@ exports.register = async (req, res) => {
     const { name, email, password, role } = req.body;
 
     try {
+        // Hash the password with bcryptjs
         const hashedPassword = await bcrypt.hash(password, 10);
         const newUser = new User({ name, email, password: hashedPassword, role });
         await newUser.save();
@@ -25,9 +26,11 @@ exports.login = async (req, res) => {
         const user = await User.findOne({ email });
         if (!user) return res.status(404).json({ message: 'User not found!' });
 
+        // Compare the password with bcryptjs
         const isMatch = await bcrypt.compare(password, user.password);
         if (!isMatch) return res.status(400).json({ message: 'Invalid credentials!' });
 
+        // Generate a JWT token
         const token = jwt.sign({ id: user._id }, process.env.JWT_SECRET, { expiresIn: process.env.JWT_EXPIRY });
         res.status(200).json({ token });
     } catch (error) {
